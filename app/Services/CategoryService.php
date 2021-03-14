36f38ccb633e7783a9606d597ad926164ba7d31b
5baddi <project@baddi.info>
2021-03-14 18:41:05 +0100

    Update category service

--- app/Services/CategoryService.php
+++ app/Services/CategoryService.php
@@ -2,7 +2,11 @@
 
 namespace App\Services;
 
+use Exception;
+use App\Models\Category;
 use InvalidArgumentException;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Facades\Validator;
 use App\Repositories\CategoriesRepository;
 
@@ -29,7 +33,7 @@ class CategoryService
      * Create new category
      *
      * @param array $data
-     * @throws InvalidArgumentException
+     * @throws \InvalidArgumentException
      */
     public function create(array $data)
     {
@@ -44,5 +48,36 @@ class CategoryService
             throw new InvalidArgumentException($validator->errors()->first());
 
         return $this->categoryRepository->create($data);
+    }
+
+    /**
+     * Delete category
+     *
+     * @param \App\Models\Category $category
+     * @return boolean
+     * @throws \Exception
+     */
+    public function delete(Category $category)
+    {
+        // Start DB transaction
+        DB::beginTransaction();
+
+        try{
+            // Delete category entity
+            $deleted = $this->categoryRepository->delete($category);
+        }catch(Exception $ex){
+            // Rollback if something going wrong
+            DB::rollBack();
+            // Trace error
+            Log::error("Unable to delete category ID:{$category->id} details: {$ex->getMessage()}");
+
+            // Throw exception
+            throw new Exception("Unable to delete this category!");
+        }
+
+        // Commit DB transaction
+        DB::commit();
+
+        return $deleted;
     }
 }
