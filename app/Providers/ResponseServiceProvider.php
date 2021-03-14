a0959a41939f470cc31e9e04a4299b50a5e31f37
5baddi <project@baddi.info>
2021-03-14 12:14:51 +0100

    Implement custom response provider

---
+++ app/Providers/ResponseServiceProvider.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Providers;
+
+use Illuminate\Support\ServiceProvider;
+
+class ResponseServiceProvider extends ServiceProvider
+{
+    /**
+     * Register services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+
+    /**
+     * Bootstrap services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        // Success response
+        $factory->macro('success', function($message = '', $data = [], $httpStatusCode = 200) use ($factory){
+            $formatedResponse = [
+                'success'   =>  true,
+                'message'   =>  $message,
+                'content'   =>  $data
+            ];
+
+            return $factory->make($formatedResponse, $httpStatusCode);
+        });
+        
+        // Error response
+        $factory->macro('error', function($message = '', $errors = [], $httpStatusCode = 500) use ($factory){
+            $formatedResponse = [
+                'success'   =>  false,
+                'message'   =>  $message,
+                'errors'    =>  $errors
+            ];
+
+            return $factory->make($formatedResponse, $httpStatusCode);
+        });
+    }
+}
